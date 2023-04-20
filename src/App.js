05da8ae9e0e372521116bd00d57e85c0be53f667
Lukas Prochazka <prochazka.lu@gmail.com>
2023-04-20 16:40:26 +0200

    Fix dependencies

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 export default function App() {
-  // 2. useState hook: reactive value > change value whenever we want.
-  // 2.1 Using randomColor npm to generate random HEX codes. -> initial value (to create function to change that value)
   const [color, setColor] = useState(randomColor());
   return (
     <div>
@@ -16,11 +14,7 @@ export default function App() {
           justifyContent: 'center',
           display: 'flex',
           alignItems: 'center',
-
-          // 3. Input color var in the style to make the background change according to click.
           backgroundColor: color,
-          fontFamily: 'Futura',
-          fontSize: 25,
           borderRadius: 10,
           width: 800,
           height: 500,
